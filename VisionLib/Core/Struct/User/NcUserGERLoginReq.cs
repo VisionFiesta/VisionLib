9e2606c2bc655250fbdf649a292a7253b1e75c06
Banks T <btrout.dhrs@gmail.com>
2020-04-28 21:45:44 -0400

    Add GER login struct

---
+++ VisionLib/Core/Struct/User/NcUserGERLoginReq.cs
@@ -0,0 +1,44 @@
+﻿using VisionLib.Common;
+using VisionLib.Common.Networking;
+using VisionLib.Core.Stream;
+
+namespace VisionLib.Core.Struct.User
+{
+    public class NcUserGERLoginReq : NetPacketStruct
+    {
+        public string Username;
+        public string Password;
+        public string SpawnApps;
+
+        public NcUserGERLoginReq(string username, string password)
+        {
+            Username = username;
+            Password = password;
+            SpawnApps = "xD";
+        }
+
+        public override int GetSize()
+        {
+            return 54;
+        }
+
+        public override void Read(ReaderStream reader)
+        {
+            Username = reader.ReadString(NameN.Name18ByteLen);
+            Password = reader.ReadString(NameN.Name4Len);
+            SpawnApps = reader.ReadString(NameN.Name5Len);
+        }
+
+        public override void Write(WriterStream writer)
+        {
+            writer.Write(Username, NameN.Name18ByteLen);
+            writer.Write(Password, NameN.Name4Len);
+            writer.Write(SpawnApps, NameN.Name5Len);
+        }
+
+        public override FiestaNetCommand GetCommand()
+        {
+            return FiestaNetCommand.NC_USER_GER_LOGIN_REQ;
+        }
+    }
+}
